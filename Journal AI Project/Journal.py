e24e2197efd81f752b763a510c6d71b3006c9f4e
Nathaniel Olguin <business.nateolguin@gmail.com>
2025-12-16 15:06:11 -0600

    1st update
    COMPLETED:
       -   Added space function 
       -   Improved date formatting for journal entries.
       -   Completed the basic file creation in STEP 2 (named with the formatted date)
    
    NEXT STEPS:
       -   fix the file spam with the current file creation
       -   create directories (Reports, Data)

--- Journal AI Project/Journal.py
+++ Journal AI Project/Journal.py
@@ -11,21 +11,34 @@ import shutil
 from datetime import datetime
 
 
+#BASE DEF
+def space():
+    for count in range(2):
+        print()
+
+
+#VARIABLES
+date = datetime.now()
+formatted_date = date.strftime('%m-%d-%Y  -  %A %I.%M.%S %p')    #including the '%S' is important as with each passing second the file name changes and therefore if the user enters multiple 'journal entries' in quick succession multiple files will be created for the same day (I may need to update this with the 'a', append, in the future to avoid creating a massive amount of files
+#need to create a new date format for the 'a', append, feature to avoid spam
+
+
 #STEP 1  |  user input
 try:
     daily_entry = input()
 except EOFError:    #enter with nothing
     daily_entry = ''
 finally:
-    print(datetime.now())
-    for count in range(2):
-        print()
+    print(formatted_sdate)
+    space()
     print(daily_entry)
-    for count in range(2):
-        print()
+    space()
 
+    
 
-#STEP 2  |  creating an 'entry file'  |  WORK-IN-PROGRESS
-print(os.name)
-cwd = os.getcwd
-print(cwd)
+#STEP 2  |  creating an 'entry file'
+initial_directory = os.getcwd()
+print(initial_directory)    #for reference
+
+with open(f'Daily Journal  -  {formatted_date}.md', 'w') as entry:
+    entry.write(f'#{formatted_date} \n \n{daily_entry}')
