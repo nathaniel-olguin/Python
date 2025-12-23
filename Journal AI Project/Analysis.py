5fd6ae071c3ff60a2853994ec966507e6886e8ab
Nathaniel Olguin <business.nateolguin@gmail.com>
2025-12-22 23:35:39 -0600

    2nd Update
    2nd Update
    
    
    COMPLETED:
       -   Created a character count function
       -   analysis.py now reads the 'data' directory files and provides an 'entries' count as well as the character count. 
       -   Fix the missing 'import os' in the prior commit
       -   Improved Python knowledge
       -   Step 2 progress
    
    
    NEXT STEPS:
       -   Creating a word count
       -   Creating a word frequency count
    
    
    NOTES:
       -   At first I struggled with getting the accurate character count. Without the .count() string method I was attempting to over-engineer a for loop. Studying and further research led me to the .count() method which made the entire function simple. 
       -   The .count() method will make the next steps easy to complete.

--- Journal AI Project/Analysis.py
+++ Journal AI Project/Analysis.py
@@ -7,12 +7,25 @@
 #    saving a report into reports
 
 
-
 #IMPORTS
-from datetime import datetime    #not currently in use
+import os
+from datetime import datetime
+
+
+#FUNCTIONS
+def character_count(file):    #the entry-dating formating is 45-ish (spacing and \n lines) characters long
+    global simple_date
+    timestamp_count = file.count(simple_date)    #counting the amount of times my entry-datetime formatting is present
+    final_count = len(file) - (timestamp_count * round(45.5))    #wordcount - the number of timestamps
+    print()    #space
+    print(f'Journal Entries:      {timestamp_count}')    #number of times a journal entry was made calculated using the count of timestamps
+    return f'Character Count:  {final_count}'    #word count
 
 
 #VARIABLES
+date = datetime.now()    #used for simple_date formatting
+simple_date = date.strftime('%m-%d-%Y')    #used for locating each timestamp per entry
+
 initial_directory = os.getcwd()    #reference
 data_dir = f'{initial_directory}\\Data'    #directory that holds the files analysis.py will read
 data_dir_list = os.listdir(data_dir)
@@ -20,11 +33,15 @@ data_dir_list = os.listdir(data_dir)
 
 #STEP 1  |  Read the latest journal 'Data' entry
 print(data_dir_list)    #reference
+
 if len(data_dir_list) < 2:    #if the 'Data' directory only has 1 entry read that entry
     with open(f'{data_dir}\\{data_dir_list[0]}', 'r') as data:
         content = data.read()
-        print(content)
-else:    #if the 'Data' directory only has more than 1 entry read the latest(last) entry
+else:    #if the 'Data' directory has more than 1 entry read the latest(last) entry
     with open(f'{data_dir}\\{data_dir_list[-1]}', 'r') as data:
         content = data.read()
-        print(content)
+print(content)
+
+
+#STEP 2  |  Anaylize the 'content'
+print(character_count(content))
