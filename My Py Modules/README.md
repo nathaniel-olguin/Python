8b2378e8569632b27a8b4f4ceb80ce4391ca253e
Nathaniel Olguin <business.nateolguin@gmail.com>
2026-01-02 00:01:20 -0600

    1st Update
    UPDATES:
       -   Optimize punctuation_checker   |   Prior version would not accurately keep count of any words that did not contain any of the undesired symbols/punctuation. This fix was achieved by adjusting when and where to use:  final_word = ''.join(rebuilt_word)  in the syntax
       -   added the function: timestamp_checker   |   used to located if any timestamps with this formatting:  MM-DD-YYYY
       -   Optimize character_count & word_count   |    both functions were flawed without having the timestamp_checker function directly hard-coded these functions were unable to detect the timestamps.
       -   Added line function   |   for personal readability in the idle shell
    
    
    NEXT STEPS:
       -   count the top most used words within the word_frequency function

--- My Py Modules/README.md
+++ My Py Modules/README.md
@@ -12,21 +12,35 @@
 
 
 
-#####    ▷   word\_count(file)
+#####    ▷   word\_count(file)
 
-               > when calling upon the function, provide a text file or string argument for the 'file' parameter.
-
-
-
-#####    ▷   word\_frequency(file)
-
-               > when calling upon the function, provide a text file or string argument for the 'file' parameter.
+               > when calling upon the function, provide a text file or string argument for the 'file' parameter.
 
 
 
-#####    ▷  punctuation\_checker(word)
+#####    ▷   word\_frequency(file)
 
-               > when calling upon the function, string argument for the 'word' parameter.
+               > when calling upon the function, provide a text file or string argument for the 'file' parameter.
+
+
+
+#####    ▷  punctuation\_checker(word)
+
+               > when calling upon the function, provide a single string argument for the 'word' parameter. 
+               > You can iterate through multiple words(strings) by utilizing a list along side the .pop() method
+               > Returns a single word(string)
+
+
+
+#####    ▷  timestamp\_checker(file)    
+
+               > ***ONLY*** used within other functions at this time. Not currently meant to be used outside of njro.py.
+
+
+
+#####    ▷  line()  
+
+               > simply call upon it, no argument required
 
 
 
@@ -36,30 +50,41 @@
 
 
 
-#####    ▷   character\_count(file)
+#####    ▷   character\_count(file)
 
-               > returns the character count(including spaces) of the provided text file or string.
-
-
-
-#####    ▷   word\_count(file)
-
-               > returns the word count of the provided text file or string.
+               > returns the character count(including spaces) of the provided text file or string.
 
 
 
-#####    ▷   word\_frequency(file)
+#####    ▷   word\_count(file)
 
-               > returns a dictionary with the number of times each word in the provided text file or string is used.
+               > returns the word count of the provided text file or string.
 
 
 
-#####    ▷  punctuation\_checker(word)
+#####    ▷   word\_frequency(file)
 
-               > returns the word provided without any punction or symbols (excluding: the '\&' sign, apostrophes, and integers).
+               > returns a dictionary with the number of times each word in the provided text file or string is used.
 
-               > if the '\&' sign is detected it will convert the word into 'and'
 
+
+#####    ▷  punctuation\_checker(word)
+
+               > returns the word provided without any punction or symbols (excluding: the '\&' sign, colons, percentage symbols, dollar signs, apostrophes, and integers).
+
+               > if the '\&' sign is detected it will convert the word into 'and'
+
+
+
+#####    ▷  timestamp\_checker(file)    
+
+               > used to verify if there is a timestamp present based on this formatting: MM-DD-YYYY
+               
+
+
+#####    ▷  line()  
+
+               > for working in the pythong idle shell | FOR READABILITY
 
 
 
--- My Py Modules/njro.py
+++ My Py Modules/njro.py
@@ -5,29 +5,36 @@
 from datetime import datetime
 
 
-#FUNCTIONS
-def character_count(file):    #the entry-dating formating is 45-ish (spacing and \n lines) characters long
+#FUNCTIONS   |   TEXT RELATED TOOLS
+def timestamp_checker(file):    #***used in the word & character count functions***
     date = datetime.now()    #used for simple_date formatting
     simple_date = date.strftime('%m-%d-%Y')    #used for locating timestamps
+    timestamp_count = file.count(simple_date)
+    return timestamp_count
 
-    timestamp_count = file.count(simple_date)    #counting the amount of times my entry-datetime formatting is present
+
+def character_count(file):    #the entry-dating formating is 45-ish (spacing and \n lines) characters long    |   ***REQUIRES A STRING ARGUMENT***
+    timestamp_count = timestamp_checker(file)
+    
     final_count = len(file) - (timestamp_count * round(45.5))    #wordcount - the number of timestamps
     print()    #space
-    print(f'Journal Entries:      {timestamp_count}')    #number of times a journal entry was made calculated using the count of timestamps
+    if timestamp_count > 0:    #if i reuse this function and there is no timestamp count no need to mention it.
+        print(f'Journal Entries:      {timestamp_count}')    #number of times a journal entry was made calculated using the count of timestamps
     return f'Character Count:  {final_count}'    #word count
 
-def word_count(file):    #word count of the timestamp is 5
-    date = datetime.now()    #used for simple_date formatting
-    simple_date = date.strftime('%m-%d-%Y')    #used for locating timestamps
 
-    timestamp_count = file.count(simple_date)
+def word_count(file):    #word count of the timestamp is 5    |   ***REQUIRES A STRING ARGUMENT***
+    timestamp_count = timestamp_checker(file)
+
     word_list = file.split()    #list of words in file
     final_count = len(word_list) - (timestamp_count * 5)
     print()    #space
-    print(f'Journal Entries:      {timestamp_count}')
-    return f'Word Count:             {final_count}'
+    if timestamp_count > 0:    #if i reuse this function and there is no timestamp count no need to mention it.
+        print(f'Journal Entries:      {timestamp_count}')
+    return f'Word Count:            {final_count}'
 
-def word_frequency(file):    #***NEEDS WORK***
+
+def word_frequency(file):    #how many times a word is used    |   ***REQUIRES A STRING ARGUMENT***
     frequency = {}    #dictionary for words used
     word_list = file.split()    #list of words in file
     while len(word_list) > 0:
@@ -41,30 +48,35 @@ def word_frequency(file):    #***NEEDS WORK***
     print('Word Frequency Data:')
     for key in frequency:    #prnt items in dictonary
         print(key , frequency[key])
-    print(word_list)    #for dev reference
 
-def punctuation_checker(word):    #Currently ignores: &, integers, hyphens, and apostrophes
+
+def punctuation_checker(word):    #***REQUIRES A STRING ARGUMENT***    Must insert a single string. Utilize a while loop and the .pop() method for lists
     split_word = []    #list that holds each individual character of a word.
-    new_word = []    #list of newly rebuilt words
-    if word.isalnum() is False:    #checking for punctuation 
-        print('***Punctuation Detected')
+    rebuilt_word = []    #list of newly rebuilt words
+    word = word.lower()    #set the string argument to lowercase
+    
+    if word.isalnum() is False:    #checking for punctuation: .isalnum() returns False if punctuation/symbols are detected
+        #***Punctuation Detected***
         for char in word:    #loop that places each character of a word into the split_word list in order.
-            split_word.append(char)
-            print(split_word)
-        #now i need to go thru each item in the list to see if its alpha/num to then rebuild the word without punctuation
-        while len(split_word) > 0:
+            split_word.append(char)   
+        while len(split_word) > 0:    #now go thru each character in the list to see if contains any undesired symbols/punctuation to then rebuild the word without them
             char = split_word.pop(0)    #remove each character from the split_word list for testing
             if char == '&':    #checking for & symbol to replace with string 'and'
-                new_word.append('and')
-            else:
-                print('No "&" Detected')
-            if char not in ('&', '!',  '@', '#', '^', '*', '(', ')', '_', '+', '=', '"', '~', '`', '[', ']', '{', '}', '|', '?', ',', '\\', '/', '<', '>', ':', ';', '.'):    #checking for certain punctuation 
-                new_word.append(char)
-                print(new_word)            
-            else:
-                print('***PUNCTUATION FOUND')    
+                rebuilt_word.append('a')
+                rebuilt_word.append('n')
+                rebuilt_word.append('d')
+                final_word = ''.join(rebuilt_word)    #rebuild the word with the .join() method        
+            if char not in ('&', '!',  '@', '#', '^', '*', '(', ')', '_', '+', '=', '"', '~', '`', '[', ']', '{', '}', '|', '?', ',', '\\', '/', '<', '>', ';', '.', ' ', ''):    #checking for certain punctuation 
+                rebuilt_word.append(char)
+                final_word = ''.join(rebuilt_word)    #rebuild the word with the .join() method           
     else:
-        print('***NO Punctuation Detected')
-        
-    final_word = ''.join(new_word)
-    return f' ***Word without punctuation:  {final_word}'
+        #***NO Punctuation Detected***
+        final_word = word
+    return final_word    #outputs a single word
+
+
+#FUNCTIONS   |   DEVELOPER BASICS (simple repeatable stuff for working in the pythong shell)
+def line():
+    print('''
+——————————————————————————————————
+''')
